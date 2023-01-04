6398806c69ad72535e6f686ed5b41ba4942890a7
Bowtiedpyro <bowtiedpyro@gmail.com>
2023-01-04 16:48:44 +0100

    add bonk setup

--- components/BidsList/BidsList.tsx
+++ components/BidsList/BidsList.tsx
@@ -4,7 +4,7 @@ import SidebarScroll from '../SidebarScroll/SidebarScroll';
 import { CurrentBidCardProps } from '../CurrentBidCard/types';
 import CurrentBidList from '../CurrentBidList/CurrentBidList';
 import { BidListProps } from './types';
-import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { BONK_DECIMAL_DIVIDER } from 'constants/market';
 
 const BidsList = (props: BidListProps) => {
   const { biddingArray, fetchedSolPrice } = props;
@@ -21,8 +21,8 @@ const BidsList = (props: BidListProps) => {
         id: index,
         date: 1663663018156,
         walletAddress: bid.bidder,
-        usdcValue: bid.bidLimit / LAMPORTS_PER_SOL,
-        solAmount: bid.bidLimit / LAMPORTS_PER_SOL
+        usdcValue: bid.bidLimit / BONK_DECIMAL_DIVIDER,
+        solAmount: bid.bidLimit / BONK_DECIMAL_DIVIDER
       };
     });
 
@@ -31,22 +31,25 @@ const BidsList = (props: BidListProps) => {
   // set bidding array to either bidding array or empty array
   useEffect(() => {
     if (biddingArray.length) {
-      handleConvertion(biddingArray)
+      handleConvertion(biddingArray);
     } else {
       handleConvertion([]);
-    };
+    }
   }, [biddingArray]);
-  // set bidCardData equal to current bidding array 
+  // set bidCardData equal to current bidding array
   const currentBidCardData: CurrentBidCardProps[] = convertedBiddingArray;
 
   return (
     <SidebarScroll>
       <div className={styles.bidsList}>
-        {
-          currentBidCardData.length ?
-          <CurrentBidList data={currentBidCardData} fetchedSolPrice={fetchedSolPrice} />
-          : 'No open bids'
-        }
+        {currentBidCardData.length ? (
+          <CurrentBidList
+            data={currentBidCardData}
+            fetchedSolPrice={fetchedSolPrice}
+          />
+        ) : (
+          'No open bids'
+        )}
       </div>
     </SidebarScroll>
   );
